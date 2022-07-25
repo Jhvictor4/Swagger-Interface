1495bdc0aae8af63cf6d51ebbeee80b4c8b1e4aa
강지혁 <54717129+Jhvictor4@users.noreply.github.com>
2022-07-25 17:31:31 +0900

    Initial commit

---
+++ src/main/kotlin/com/jhvictor4/swaggerit/SwaggerInterfaceTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.jhvictor4.swaggerit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwaggerInterfaceTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwaggerInterfaceTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/jhvictor4/swaggerit/SwaggerInterfaceTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jhvictor4.swaggerit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwaggerInterfaceTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
